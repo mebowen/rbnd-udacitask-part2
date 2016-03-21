d86a422ae550bacd568b43bb80eacc5038a6e916
marlenaonrails <marlenabowen@gmail.com>
2016-03-21 17:49:07 +0000

    accidentally corrupted git repo, starting from scratch with current progress to project. changes to original code include addition of chronic & colorize gems, completion of errors.rb, completion of add method in Udacilist, and creation of Listable module

--- lib/listable.rb
+++ lib/listable.rb
@@ -1,3 +1,25 @@
 module Listable
-  # Listable methods go here
+  def format_description(description)
+      "#{description}".ljust(30)
+  end
+  def format_date(due, event = nil)
+      if event == nil 
+          due ? due.strftime("%D") : "No due date"
+      else
+          dates = @start_date.strftime("%D") if @start_date
+          dates << " -- " + @end_date.strftime("%D") if @end_date
+          dates = "N/A" if !dates
+          return dates
+      end
+  end
+  def format_priority
+    value = " ⇧".colorize(:red) if @priority == "high"
+    value = " ⇨".colorize(:yellow) if @priority == "medium"
+    value = " ⇩".colorize(:green) if @priority == "low"
+    value = "" if !@priority
+    return value
+  end
+  def format_name(site_name)
+    @site_name ? @site_name : ""
+  end
 end
--- lib/udacilist.rb
+++ lib/udacilist.rb
@@ -5,15 +5,38 @@ class UdaciList
     @title = options[:title]
     @items = []
   end
+  
   def add(type, description, options={})
     type = type.downcase
-    @items.push TodoItem.new(description, options) if type == "todo"
-    @items.push EventItem.new(description, options) if type == "event"
-    @items.push LinkItem.new(description, options) if type == "link"
+    if ["todo", "event", "link"].include?(type)
+      if type == "todo"
+        if options.key?(:priority)
+           if ["low", "medium", "high"].include?(options[:priority])
+             @items.push TodoItem.new(description, options) 
+           else
+             raise UdaciListErrors::InvalidPriorityValue
+           end
+        else 
+          @items.push TodoItem.new(description, options) 
+        end
+      elsif type == "event"
+        @items.push EventItem.new(description, options)
+      elsif type == "link"
+        @items.push LinkItem.new(description, options)
+      end
+    else
+      raise UdaciListErrors::InvalidItemType
+    end
   end
+  
   def delete(index)
-    @items.delete_at(index - 1)
+    if index >= 0 and index < items.length 
+      @items.delete_at(index - 1)
+    else
+      raise UdaciListErrors::IndexExceedsListSize
+    end
   end
+  
   def all
     puts "-" * @title.length
     puts @title
